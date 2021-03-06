c188ab55e1a865be39bff3206af08c4595e83787
luoxuliang <962526394@qq.com>
2021-03-06 10:49:38 +0800

    Update

--- app/src/main/java/com/example/androiddevchallenge/ui/CountDownTimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/CountDownTimerScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui
 
 import androidx.compose.animation.core.Animatable
@@ -37,9 +52,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androiddevchallenge.formatTime
 import com.example.androiddevchallenge.ui.theme.RunningState
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 /**
  *  @author: LXL
@@ -212,10 +227,10 @@ fun CircularCountDownTimer(
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
                     text = "${formatTime(isLeadingZeroNeeded = true, value = leftTime / 3600)}:" +
-                            "${
-                                formatTime(isLeadingZeroNeeded = true, value = (leftTime / 60) % 60)
-                            }:" +
-                            formatTime(isLeadingZeroNeeded = true, value = leftTime % 60),
+                        "${
+                        formatTime(isLeadingZeroNeeded = true, value = (leftTime / 60) % 60)
+                        }:" +
+                        formatTime(isLeadingZeroNeeded = true, value = leftTime % 60),
                     fontSize = 48.sp
 
                 )
